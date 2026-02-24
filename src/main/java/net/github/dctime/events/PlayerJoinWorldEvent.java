3143b8c318f9adc9ba5c761c56df8d26be59de28
DCtime <daniel.chu916@gmail.com>
2026-02-24 21:11:49 +0800

    一點沒注意到的

--- src/main/java/net/github/dctime/events/PlayerJoinWorldEvent.java
+++ src/main/java/net/github/dctime/events/PlayerJoinWorldEvent.java
@@ -17,13 +17,13 @@ public class PlayerJoinWorldEvent {
     public static void onLocalPlayerJoinLevel(EntityJoinLevelEvent event) {
         if (!loginHandled && event.getEntity() == Minecraft.getInstance().player) {
             loginHandled = true;
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal("感謝使用 Google AI Studio Translator! 自動翻譯提示匡, Jade, Advancements 與 FTBQuest 的內容的小工具!").withStyle(net.minecraft.ChatFormatting.GREEN));
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal("感謝使用 Microdaery's Translator! 自動翻譯提示匡, Jade, Advancements 與 FTBQuest 的內容的小工具!").withStyle(net.minecraft.ChatFormatting.GREEN));
             Minecraft.getInstance().player.sendSystemMessage(Component.literal("螢幕截圖會把裡面的英文文字翻成中文顯示在對話框").withStyle(net.minecraft.ChatFormatting.GREEN));
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal("使用前請先去修改 config/googleaistudiotranslator-client.toml").withStyle(net.minecraft.ChatFormatting.GREEN));
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal("使用前請先去修改 config/microdaerystranslator-client.toml").withStyle(net.minecraft.ChatFormatting.GREEN));
             Minecraft.getInstance().player.sendSystemMessage(Component.literal("按F4可以清除翻譯快取 按F6可以重新翻譯正在顯示的翻譯 可去按鍵設定修改").withStyle(net.minecraft.ChatFormatting.GREEN));
             Minecraft.getInstance().player.sendSystemMessage(Component.literal("在GUI裡按Left Alt可以顯示最近翻譯的螢幕截圖 可去按鍵設定修改").withStyle(net.minecraft.ChatFormatting.GREEN));
             Minecraft.getInstance().player.sendSystemMessage(Component.literal("如果找到bug或是想要什麼請").withStyle(net.minecraft.ChatFormatting.GREEN));
-            Minecraft.getInstance().player.sendSystemMessage(Component.literal("點這裡").withStyle(s -> s.withColor(net.minecraft.ChatFormatting.GREEN).withUnderlined(true).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/dctime/GoogleAIStudioTranslatorMod/issues"))));
+            Minecraft.getInstance().player.sendSystemMessage(Component.literal("點這裡").withStyle(s -> s.withColor(net.minecraft.ChatFormatting.GREEN).withUnderlined(true).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/dctime/MicrodaerysTranslatorMod/issues"))));
         }
     }
 
